db20e280c7cbe83d440c37ad03f7ad5220d9087a
Gianmarco De Francisci Morales <gdfm+github@gdfm.me>
2013-03-11 15:41:53 +0100

    added pseudo-test

---
+++ src/test/java/com/yahoo/research/bcn/TwitterCrawlerTest.java
@@ -0,0 +1,29 @@
+package com.yahoo.research.bcn;
+
+import static org.junit.Assert.*;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.StringReader;
+import java.io.StringWriter;
+
+import org.junit.Test;
+
+public class TwitterCrawlerTest {
+    private static final String USER_ID = "234017215";
+
+    @Test
+    public void test() throws IOException {
+        StringWriter sw = new StringWriter();
+        BufferedReader in = new BufferedReader(new StringReader(USER_ID));
+        PrintWriter out = new PrintWriter(sw);
+        TwitterCrawler crawler = new TwitterCrawler(in, out);
+        crawler.nextUser();
+        crawler.crawlNextFriends();
+        crawler.close();
+        String results = sw.toString();
+        assertTrue("No results", results.length() > 0);
+        System.out.println(results);
+    }
+}
